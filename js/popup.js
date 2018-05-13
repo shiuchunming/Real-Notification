9ac3d5bd33ef5140d103a441e8b44d6542929b42
SHIU Chun Ming <hugoshiu1222@gmail.com>
2018-05-13 23:21:06 +0800

    m

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener(messageReceived);
 
 var notification = null;
+var rate = 0;
 var message_json = {
     "facebook": [],
     "whatsapp": []
@@ -13,7 +14,7 @@ function messageReceived(msg) {
         if (message_json.facebook[message_json.facebook.length - 1][1] != msg.greeting[1]) {
             message_json.facebook.push(msg.greeting);
             localStorage.setItem("messages", JSON.stringify(message_json));
-            
+
             notification = new Notification(new Date(), {
                 icon: "https://image.freepik.com/free-icon/facebook-logo_318-49940.jpg",
                 body: msg.greeting[1]
@@ -32,7 +33,7 @@ function messageReceived(msg) {
         setTimeout(notification.close.bind(notification), 4000);
     }
 }
-
+``
 setInterval(function(){
     message_json = JSON.parse(localStorage.getItem("messages") || '{"facebook": [], "whatsapp": []}');
     var content = "";
@@ -55,6 +56,10 @@ setInterval(function(){
             <span>
             ` + "&nbsp&nbsp&nbsp&nbsp&nbsp sender: " + message_json.facebook[id_1][0] + `
             </span> 
+            <span>
+                &nbsp;
+                <button id="control" type="button" class="btn btn-info btn-sm">Control</button>
+            </span>
             </p>
             </div>
             `;
@@ -63,10 +68,10 @@ setInterval(function(){
         id_2 += 1;
     }
     $('.messages').html(content);
-}, 0)
+}, rate)
+
 
 $(function () {
-    
     $('.reminder').keypress(function (e) {
         if (e.which == 13) {
             chrome.extension.sendMessage({ msg: "reset_timer" });
@@ -82,4 +87,32 @@ $(function () {
             $('#remind-sec').blur();
         }
     })
+
+    $('.spinner .btn:first-of-type').on('click', function () {
+        var time = parseInt($('.spinner input').val(), 10) + 1;
+        $('.spinner input').val(time);
+    });
+
+    $('.spinner .btn:last-of-type').on('click', function () {
+        var time = parseInt($('.spinner input').val(), 10) - 1;
+        $('.spinner input').val(time);
+    });
+
+    // $(".container").click(function () {
+    //     console.log("DEBUG::click");
+    //     rate = 9999999;
+    //     content = `
+    //         <img src="https://image.freepik.com/free-icon/facebook-logo_318-49940.jpg" alt="Avatar" >         
+    //         <div class="input-group spinner">
+    //             <input class="form-control form-control-sm" id="inputdefault" value="30" type="text">
+    //             <div class="input-group-btn-vertical">
+    //                 <button class="btn btn-default" type="button"><i class="fa fa-caret-up"></i></button>
+    //                 <button class="btn btn-default" type="button"><i class="fa fa-caret-down"></i></button>
+    //             </div>
+    //             <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+    //             <button type="button" class="btn btn-default">Return</button>
+    //         </div>
+    //     `;
+    //     $(".container").html(content);
+    // });
 })
