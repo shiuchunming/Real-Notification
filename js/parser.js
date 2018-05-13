347e11894ec8a4e10216b916ff3da672992c9e60
SHIU Chun Ming <hugoshiu1222@gmail.com>
2018-05-13 21:31:37 +0800

    m

--- js/parser.js
+++ js/parser.js
@@ -26,9 +26,9 @@ const Parser = (() => {
                 // console.log("Name: "+ response.querySelector("#messages_flyout > div > ol").getElementsByTagName("li")[i+2].querySelector("a > div > div.content > div.lr > div.title.thread-title.mfsl.fcb > strong").innerText);
                 // console.log("Conetent: "+ response.querySelector("#messages_flyout > div > ol").getElementsByTagName("li")[i+2].querySelector("a > div > div.content > div.oneLine.preview.mfss.fcg > span").innerText);
                 // console.log("Time: "+response.querySelector("#messages_flyout > div > ol").getElementsByTagName("li")[i+2].querySelector("a > div > div.content > div.lr > div.time.r.nowrap.mfss.fcl > abbr").innerText);
-            list.push(response.querySelector("#messages_flyout > div > ol").getElementsByTagName("li")[i+2].querySelector("a > div > div.content > div.lr > div.title.thread-title.mfsl.fcb > strong").innerText);
-            list.push(response.querySelector("#messages_flyout > div > ol").getElementsByTagName("li")[i+2].querySelector("a > div > div.content > div.oneLine.preview.mfss.fcg > span").innerText);
-            list.push(response.querySelector("#messages_flyout > div > ol").getElementsByTagName("li")[i+2].querySelector("a > div > div.content > div.lr > div.time.r.nowrap.mfss.fcl > abbr").innerText);
+            list.push(response.querySelector("#messages_flyout > div > ol").getElementsByTagName("li")[2].querySelector("a > div > div.content > div.lr > div.title.thread-title.mfsl.fcb > strong").innerText);
+            list.push(response.querySelector("#messages_flyout > div > ol").getElementsByTagName("li")[2].querySelector("a > div > div.content > div.oneLine.preview.mfss.fcg > span").innerText);
+            list.push(response.querySelector("#messages_flyout > div > ol").getElementsByTagName("li")[2].querySelector("a > div > div.content > div.lr > div.time.r.nowrap.mfss.fcl > abbr").innerText);
             // }
             status.set_counts(counts);
             
--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,6 @@
 chrome.runtime.onMessage.addListener(messageReceived);
 
+var notification = null;
 var message_json = {
     "facebook": [],
     "whatsapp": []
@@ -7,51 +8,64 @@ var message_json = {
 
 function messageReceived(msg) {
     message_json = JSON.parse(localStorage.getItem("messages") || '{"facebook": [], "whatsapp": []}');
-    console.log(msg.greeting);
+
     if (message_json.facebook.length > 0) {
         if (message_json.facebook[message_json.facebook.length - 1][1] != msg.greeting[1]) {
             message_json.facebook.push(msg.greeting);
             localStorage.setItem("messages", JSON.stringify(message_json));
+            
+            notification = new Notification(new Date(), {
+                icon: "https://image.freepik.com/free-icon/facebook-logo_318-49940.jpg",
+                body: msg.greeting[1]
+            });
+            setTimeout(notification.close.bind(notification), 4000);
         }
     }
     else if (message_json.facebook.length == 0) {
         message_json.facebook.push(msg.greeting);
         localStorage.setItem("messages", JSON.stringify(message_json));
+
+        notification = new Notification(new Date(), {
+            icon: "https://image.freepik.com/free-icon/facebook-logo_318-49940.jpg",
+            body: msg.greeting[1]
+        });
+        setTimeout(notification.close.bind(notification), 4000);
     }
 }
 
-$(function () {
-    setInterval(function(){
-        message_json = JSON.parse(localStorage.getItem("messages") || '{"facebook": [], "whatsapp": []}');
-        var content = "";
-        var id_1 = 0;
-        var id_2 = 0.1;
+setInterval(function(){
+    message_json = JSON.parse(localStorage.getItem("messages") || '{"facebook": [], "whatsapp": []}');
+    var content = "";
+    var id_1 = 0;
+    var id_2 = 0.1;
 
-        for(ele in message_json.facebook) {
-            var tmp = `
-                <div class="container" id="
-                ` + id_1 + `
-                " style="">
-                <img src="https://image.freepik.com/free-icon/facebook-logo_318-49940.jpg" alt="Avatar" >
-                <p id="
-                ` + id_2 + `
-                ">
-                ` + message_json.facebook[id_1][1] + `
-                <span>
-                ` + "&nbsp&nbsp&nbsp&nbsp&nbsp time: " + message_json.facebook[id_1][2] + `
-                </span>
-                <span>
-                ` + "&nbsp&nbsp&nbsp&nbsp&nbsp sender: " + message_json.facebook[id_1][0] + `
-                </span> 
-                </p>
-                </div>
-                `;
-            content += tmp;
-            id_1 += 1;
-            id_2 += 1;
-        }
-        $('.messages').html(content);
-    }, 0)
+    for(ele in message_json.facebook) {
+        var tmp = `
+            <div class="container" id="
+            ` + id_1 + `
+            " style="">
+            <img src="https://image.freepik.com/free-icon/facebook-logo_318-49940.jpg" alt="Avatar" >
+            <p id="
+            ` + id_2 + `
+            ">
+            ` + message_json.facebook[id_1][1] + `
+            <span>
+            ` + "&nbsp&nbsp&nbsp&nbsp&nbsp time: " + message_json.facebook[id_1][2] + `
+            </span>
+            <span>
+            ` + "&nbsp&nbsp&nbsp&nbsp&nbsp sender: " + message_json.facebook[id_1][0] + `
+            </span> 
+            </p>
+            </div>
+            `;
+        content += tmp;
+        id_1 += 1;
+        id_2 += 1;
+    }
+    $('.messages').html(content);
+}, 0)
+
+$(function () {
     
     $('.reminder').keypress(function (e) {
         if (e.which == 13) {
